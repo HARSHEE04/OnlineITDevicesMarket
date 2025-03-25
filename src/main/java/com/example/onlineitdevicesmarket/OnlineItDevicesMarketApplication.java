bb0d2ba8374dba0b82f43e1964f00ee23d77bcd6
HARSHEE04 <harshetasharma5@gmail.com>
2025-03-25 14:28:39 -0400

    Initial commit

---
+++ src/main/java/com/example/onlineitdevicesmarket/OnlineItDevicesMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineitdevicesmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineItDevicesMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineItDevicesMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineitdevicesmarket/OnlineItDevicesMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineitdevicesmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineItDevicesMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
