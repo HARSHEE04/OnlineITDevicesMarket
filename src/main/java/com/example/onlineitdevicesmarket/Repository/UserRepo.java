28ef653bc3d2167972665cd4a0d3dce5a2e3cd6b
HARSHEE04 <harshetasharma5@gmail.com>
2025-03-25 21:38:34 -0400

    UserRepo class made which extends the appliction to the MYSQL database

--- src/main/java/com/example/onlineitdevicesmarket/Repository/UserRepo.java
+++ src/main/java/com/example/onlineitdevicesmarket/Repository/UserRepo.java
@@ -1,4 +1,14 @@
 package com.example.onlineitdevicesmarket.Repository;
 
-public class UserRepo {
+import com.example.onlineitdevicesmarket.Model.Order;
+import com.example.onlineitdevicesmarket.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+    List<Order> findbyname(String name);
 }
