34b33595dc214b2f4dc5756258ac1e8acb2bce64
HARSHEE04 <harshetasharma5@gmail.com>
2025-03-25 21:36:53 -0400

    OrderRepo class file made to extend application to MYSQL database

--- src/main/java/com/example/onlineitdevicesmarket/Repository/OrderRepo.java
+++ src/main/java/com/example/onlineitdevicesmarket/Repository/OrderRepo.java
@@ -1,4 +1,14 @@
 package com.example.onlineitdevicesmarket.Repository;
 
-public class OrderRepo {
+import com.example.onlineitdevicesmarket.Model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+    List<Order> findbyuId(int uId);
 }
+
+
